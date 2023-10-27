374f2c12979902d9834bb7080d228e801bc237bb
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2023-10-27 22:32:29 +0600

    [Part-1]: Lets Know about Next Js

---
+++ nextjs-part1/src/app/(with-layout)/page.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main>
+      <h1>The React Framework for the Web</h1>
+    </main>
+  );
+};
+
+export default HomePage;
---
+++ nextjs-part1/src/app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="text-center text-red-600">
+      <h1>404</h1>
+      <h3>Not Found</h3>
+    </div>
+  );
+};
+
+export default NotFound;
